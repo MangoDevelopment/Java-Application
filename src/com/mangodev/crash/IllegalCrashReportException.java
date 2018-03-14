68b62236d130b3ddec3ed61722911b182031085a
Bodie Brewer <unconfigured@null.spigotmc.org>
2018-03-14 16:26:21 -0400

    -

--- src/com/mangodev/crash/IllegalCrashReportException.java
+++ src/com/mangodev/crash/IllegalCrashReportException.java
@@ -2,7 +2,5 @@ package com.mangodev.crash;
 
 public class IllegalCrashReportException extends SecurityException{
 	private static final long serialVersionUID = -2884845365963776671L;
-	public IllegalCrashReportException(){
-		super();
-	}
+	public IllegalCrashReportException(){ super(); }
 }
